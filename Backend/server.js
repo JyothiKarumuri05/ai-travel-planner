a3abf4a74c654ec9488e6ff7b77ca677bdcbdd4f
Jyothi Karumuri05 <keerthikarumuri97@gmail.com>
2026-04-08 11:08:43 +0530

    crs completely

--- Backend/server.js
+++ Backend/server.js
@@ -8,12 +8,13 @@ const path = require("path");
 const app = express();
 const PORT = 5000;
 
-
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  next();
+}); 
 
 app.use(express.json());
 app.use('/api', bookingRoutes);
