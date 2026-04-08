d685d86a44fd877d8bf793e0e434babde047a4a3
Jyothi Karumuri05 <keerthikarumuri97@gmail.com>
2026-04-08 09:57:30 +0530

    db updated in both

--- Backend/server.js
+++ Backend/server.js
@@ -14,11 +14,21 @@ app.use('/api', bookingRoutes);
 app.use("/generated_pdfs", express.static("generated_pdfs"));
 
 
-const db = new sqlite3.Database("/app/data/travel_storing.db", (err) => {
+// const db = new sqlite3.Database("/app/data/travel_storing.db", (err) => {
+//   if (err) console.error("DB Error:", err.message);
+//   else console.log("✅ Connected to SQLite database");
+// });
+
+const path = require("path");
+
+const dbPath = process.env.NODE_ENV === "production"
+  ? "/app/data/travel_storing.db"
+  : path.join(__dirname, "travel.db");
+
+const db = new sqlite3.Database(dbPath, (err) => {
   if (err) console.error("DB Error:", err.message);
   else console.log("✅ Connected to SQLite database");
 });
-
 
 /* ---------- CREATE TABLES ---------- */
 
