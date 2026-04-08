027a789e4662aa463dde2f8eb3ed82dec6afd774
Jyothi Karumuri05 <keerthikarumuri97@gmail.com>
2026-04-08 10:50:06 +0530

    fixed cors issue

--- Backend/server.js
+++ Backend/server.js
@@ -8,18 +8,22 @@ const path = require("path");
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+app.options("*", cors());
 app.use(express.json());
 app.use('/api', bookingRoutes);
 app.use("/generated_pdfs", express.static("generated_pdfs"));
-
 
 // const db = new sqlite3.Database("/app/data/travel_storing.db", (err) => {
 //   if (err) console.error("DB Error:", err.message);
 //   else console.log("✅ Connected to SQLite database");
 // });
 
-//const path = require("path");
 
 const dbPath = process.env.NODE_ENV === "production"
   ? "/app/data/travel_storing.db"
