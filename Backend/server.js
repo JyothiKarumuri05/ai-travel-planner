8a0b34e6779cb871e630ea302992daf93a21e7b4
Jyothi Karumuri05 <keerthikarumuri97@gmail.com>
2026-04-08 11:56:55 +0530

    savethis

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,8 @@ const { spawn } = require("child_process");
 const path = require("path");
 
 const app = express();
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
+
 
 app.use(cors());
 app.use(express.json());
@@ -389,6 +390,6 @@ app.get("/download-pdf/:request_id", (req, res) => {
   );
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running → http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
